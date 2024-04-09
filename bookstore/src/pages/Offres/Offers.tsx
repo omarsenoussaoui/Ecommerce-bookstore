fc609fa9fd6e031c51efdd4f2ef5329082d8d507
omar <omarsenoussaoui5@gmail.com>
2024-04-09 05:01:08 +0100

    offers

---
+++ bookstore/src/pages/Offres/Offers.tsx
@@ -0,0 +1,119 @@
+
+function Offers() {
+    return (
+        <>
+            <section className="suggestion">
+                <div className="container">
+                    <div className="recommend">
+                        <h4>Recommended For You</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                            exercitationem.Lorem ipsum dolor sit amet.
+                        </p>
+                        <div className="book-container">
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-1.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-2.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-3.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-4.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div className="circle-1"></div>
+                        <div className="circle-2"></div>
+                        <div className="square-dot">
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                        </div>
+                    </div>
+                    <div className="popular">
+                        <h4>Popular in 2020</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                            exercitationem.
+                        </p>
+                        <div className="book-container">
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-5.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-6.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-7.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                            <div className="book">
+                                <figure>
+                                    <a href="Pages/book-detail.html"
+                                    ><img src="images/book-8.jpg" alt=""
+                                        /></a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div className="circle-1"></div>
+                        <div className="circle-2"></div>
+                        <div className="square-dot">
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                            <i className="fa-solid fa-square"></i>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Offers
